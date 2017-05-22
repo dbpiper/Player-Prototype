e547bab02b1b2a61bb0948a3f5ea5a6f1abf2d93
David Piper <dbpiper@gmail.com>
2017-05-22 13:53:53 -0500

    Add JasperControls script and isJumping trigger to support jumping.

---
+++ Player-Prototype/Assets/Scripts/JasperControls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JasperControls : MonoBehaviour {
+
+    static Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Jump")) {
+            anim.SetTrigger("isJumping");
+        }
+	}
+}
