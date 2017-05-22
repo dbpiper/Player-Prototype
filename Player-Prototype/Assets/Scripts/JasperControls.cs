c9d653c5d5edfb8f5dd3e675d0f774cc9b7fe3d3
David Piper <dbpiper@gmail.com>
2017-05-22 14:06:58 -0500

    Add mirror and attach camera to player's head to make first-person. Add movement controls.

--- Player-Prototype/Assets/Scripts/JasperControls.cs
+++ Player-Prototype/Assets/Scripts/JasperControls.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class JasperControls : MonoBehaviour {
 
     static Animator anim;
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +15,13 @@ public class JasperControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
         if(Input.GetButtonDown("Jump")) {
             anim.SetTrigger("isJumping");
         }
